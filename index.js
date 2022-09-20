852b9359c35b17975a3892af7003003d1f751c25
Cecilia Avila <ceciliaagarzon@yahoo.com>
2022-09-19 20:36:34 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("btn").addEventListener("click", () => {
+    alert("Formulario enviado")
+})
+document.getElementById("btn1").addEventListener("click", () => {
+    alert("No hay información :(")
+})
+
